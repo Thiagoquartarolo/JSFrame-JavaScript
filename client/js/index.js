03761f3b66b09a81d8df5dfdb3e91301ca806eac
Thiago Quartarolo <thiago.quartarolo@gmail.com>
2017-04-03 19:22:51 -0300

    Ajustes de bugs

--- client/js/index.js
+++ client/js/index.js
@@ -1,29 +1,37 @@
 var campos = [
-  document.querySelector("#data"),
-  document.querySelector("#quantidade"),
-  document.querySelector("#valor")
-]
+  document.querySelector('#data'),
+  document.querySelector('#quantidade'),
+  document.querySelector('#valor')  
+];
 
-var tBody = document.querySelector("table tbody");
+console.log(campos);
 
-document.querySelector(".form").addEventListener("submit", function (event) {
-  event.preventDefault();
-  var tr = document.createElement("tr");
+var tbody = document.querySelector('table tbody');
 
-  campos.forEach(function (campos) {
-    var td = document.createElement("td");
-    td.textContent = campos.value;
-    tr.appendChild(td);
-  });
-
-  var tdVolume = document.createElement("td");
-  tdVolume.textContent = campos[1].value * campos[2].value;
+document.querySelector('.form').addEventListener('submit', function(event) {
+    
+   event.preventDefault();
+   
+   var tr = document.createElement('tr');
+   
+   campos.forEach(function(campo) {
+       
+       var td = document.createElement('td');
+       td.textContent = campo.value;
+       tr.appendChild(td);
+   });
+   
+  var tdVolume = document.createElement('td');
+  tdVolume.textContent = campos[1].value * campos[2].value; 
+  
   tr.appendChild(tdVolume);
-
-  tBody.appendChild(tr);
-
-  campos[0].value = "";
+  
+  tbody.appendChild(tr);
+  
+  campos[0].value = '';
+  campos[1].value = 1;
+  campos[2].value = 0;
+  
   campos[0].focus();
-  campos[1].value = "1";
-  campos[2].value = "0";
+   
 });
